f3b698fd92984d2c05c0eb9cec73d0cdaba30c4b
robertdurst <rsdurst@colby.edu>
2018-04-22 12:14:24 -0700

    Basic stream example for ws in rust

--- native/src/meetup/mod.rs
+++ native/src/meetup/mod.rs
@@ -1,11 +1,1 @@
-use std::env;
-use reqwest;
-
-/// Really bad rust code -- this is just the beginning :)
-pub fn get_all_events() -> Result<String, reqwest::Error> {
-    let base = "https://api.meetup.com/SF-Cryptocurrency-Devs/events";
-    let url = &format!("{}?key={}", base, &env::var("MEETUP_API_KEY").unwrap());
-    let resp = reqwest::get(url)?.text()?;
-
-    return Ok(resp);
-}
+pub mod stream;
