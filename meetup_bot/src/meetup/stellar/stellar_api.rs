7eb2cc7de4911be47d3063182b7fa98d9f73f2e2
robertdurst <rsdurst@colby.edu>
2018-04-29 22:14:31 -0700

    No more unsafe code in poll

--- meetup_bot/src/meetup/stellar/stellar_api.rs
+++ meetup_bot/src/meetup/stellar/stellar_api.rs
@@ -1,7 +1,3 @@
-// Test account
-// GCNCCXU2UI7D6KVSOEM7OY3KDD5E3VKST3XSCH2A22BEGZALC22GKTH7
-// SCDQ3EN54H4KQ5O4QO3MUFIYIJK5IYHL6Y7VYYSTBKW63QS5QZ7VMS5T
-
 use stellar_client::sync::{Client};
 use stellar_client::endpoint::{account, Limit, Order, Direction, Cursor};
 use std::time::Duration;
@@ -9,34 +5,40 @@ use futures::prelude::*;
 use futures_timer::Interval;
 use std::str::FromStr;
 
-static mut CURRENT_CURSOR: i64 = 0;
-
-pub fn poll() {
+pub fn init_poll() {
+    let mut poll = Poll{ cursor: 0};
     Interval::new(Duration::from_secs(1))
-      .for_each(|()| Ok(get_payments()))
+      .for_each(|()| Ok(poll.get_payments()))
       .wait()
       .unwrap();
 }
 
-pub fn get_payments() {
+struct Poll {
+    cursor: i64,
+}
+
+impl Poll {
+    fn get_payments(&mut self) {
+        
     // Establish whether connecting to testnet or mainnet
     let client = Client::horizon_test().unwrap();
 
-    // TODO: remove unsafe code
-    unsafe {
         let endpoint;
         // If this is the first time, get latest cursor (ie order=desc and latest is top)
-        if CURRENT_CURSOR == 0 {
-            endpoint = account::Payments::new("GCNCCXU2UI7D6KVSOEM7OY3KDD5E3VKST3XSCH2A22BEGZALC22GKTH7").with_limit(200).with_order(Direction::Desc).with_cursor("now");
+        if self.cursor == 0 {
+            endpoint = account::Payments::new("GCJY6RHN3SOUKBZXTDNWEJUSOH5PY7GV5Q44OK4BGKYHRM7EE5FXVHW7").with_limit(200).with_order(Direction::Desc).with_cursor("now");
         } else {
-            endpoint = account::Payments::new("GCNCCXU2UI7D6KVSOEM7OY3KDD5E3VKST3XSCH2A22BEGZALC22GKTH7").with_limit(200).with_order(Direction::Asc).with_cursor(&CURRENT_CURSOR.to_string());
+            endpoint = account::Payments::new("GCJY6RHN3SOUKBZXTDNWEJUSOH5PY7GV5Q44OK4BGKYHRM7EE5FXVHW7").with_limit(200).with_order(Direction::Asc).with_cursor(&self.cursor.to_string());
         }
 
         let payments = client.request(endpoint).unwrap();
+        
+        println!("{:?}", payments.records());
 
         if payments.records().len() > 0 {
             let last_cursor = payments.records()[0].paging_token();
-            CURRENT_CURSOR = FromStr::from_str(last_cursor).unwrap();
+            self.cursor = FromStr::from_str(last_cursor).unwrap();
         }
     }
 }
+
