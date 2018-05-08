5bd81dc8a12e8875e7fcf0fc8326d05221e4f8c7
robertdurst <rsdurst@colby.edu>
2018-05-07 23:40:37 -0700

    basic filtering and polling of memos for stellar transactions

--- meetup_bot/src/meetup/stellar/stellar_api.rs
+++ meetup_bot/src/meetup/stellar/stellar_api.rs
@@ -1,10 +1,13 @@
 use stellar_client::sync::{Client};
 use stellar_client::endpoint::{account, Limit, Order, Direction, Cursor};
+use stellar_client::resources::Memo;
 use std::str::FromStr;
 use std::{thread, time};
+use std::collections::HashMap;
 
 pub fn init_poll() {
-    let mut poll = Poll{ cursor: 0};
+    let mut poll = Poll::new();
+    poll.add_memo("hello", 10);
     
     loop {
         thread::sleep(time::Duration::from_millis(1000));
@@ -14,9 +17,17 @@ pub fn init_poll() {
 
 struct Poll {
     cursor: i64,
+    filter: Filter,
 }
 
 impl Poll {
+    fn new() -> Poll {
+        Poll {
+            cursor: 0,
+            filter: Filter::new(),
+        }
+    }
+
     fn get_payments(&mut self) {
         
     // Establish whether connecting to testnet or mainnet
@@ -38,12 +49,45 @@ impl Poll {
             // Update last cursor
             let last_cursor = transactions.records()[0].paging_token();
             self.cursor = FromStr::from_str(last_cursor).unwrap();
-
+            
             for node in transactions.records().iter() {
-                let memo = node.memo();
-                println!("{:?}", memo);
+                if let &Memo::Text(ref value) = node.memo() {
+                    if self.filter.memo_exists(value) {
+                        self.filter.remove_memo(value);
+                        println!("🔥 Received transaction with memo: {} 🔥", value);
+                    } else {
+                        println!("Received extraneous transaction with memo: {}", value);
+                    }
+                    
+                }           
             }
         }
+    }
+
+    fn add_memo(&mut self, memo: &str, amount: i64) {
+        self.filter.add_memo(memo, amount);
+    }
+}
+
+struct Filter {
+    memo_map: HashMap<String, i64>,
+}
+
+impl Filter {
+    fn new() -> Filter {
+        Filter {memo_map: HashMap::new()}
+    }
+    
+    fn add_memo(&mut self, memo: &str, amount: i64) {
+        &self.memo_map.insert(memo.to_string(), amount);
+    }
+
+    fn remove_memo(&mut self, memo: &str) {
+        &self.memo_map.remove(memo);
+    }
+    
+    fn memo_exists(&self, memo: &str) -> bool {
+        self.memo_map.contains_key(memo)
     }
 }
 
